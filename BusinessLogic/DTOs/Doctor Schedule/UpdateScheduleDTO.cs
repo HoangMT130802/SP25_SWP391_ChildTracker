2c422ad40a174dc2507cc9581991462bc77e3e62
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-02-26 22:34:40 +0700

    Add doctor schedule DTO

---
+++ BusinessLogic/DTOs/Doctor Schedule/UpdateScheduleDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTOs.Doctor_Schedule
+{
+    public class UpdateScheduleDTO
+    {
+        public DateOnly WorkDate { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public int SlotDuration { get; set; }
+        public string Status { get; set; }
+    }
+}

e80237b4fa7d070eb8720723ddb0fb8d650409d7
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-02-26 23:00:09 +0700

    Add interface doctorService

---
+++ BusinessLogic/Services/Interfaces/IDoctorService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic.DTOs.Doctor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<DoctorDTO>> GetAllDoctorsAsync();
+        Task<DoctorDTO> GetDoctorByIdAsync(int doctorId);
+        Task<DoctorDTO> CreateDoctorAsync(CreateDoctorDTO doctorDTO);
+        Task<DoctorDTO> UpdateDoctorAsync(int doctorId, UpdateDoctorDTO doctorDTO);
+        Task<bool> DeleteDoctorAsync(int doctorId);
+    }
+}

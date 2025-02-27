f5d3ac044264c5649ac00a3c6b6373f4a7569e30
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-02-27 10:11:35 +0700

    Create doctor schedule service. Add function of doctor schedule : Get all Schedule

---
+++ BusinessLogic/Services/Implementations/DoctorScheduleService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BusinessLogic.DTOs.Doctor_Schedule;
+using BusinessLogic.Services.Interfaces;
+using DataAccess.Entities;
+using DataAccess.UnitOfWork;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.Implementations
+{
+    public class DoctorScheduleService : IDoctorScheduleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DoctorScheduleService> _logger;
+
+        public DoctorScheduleService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<DoctorScheduleService> logger)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<IEnumerable<DoctorScheduleDTO>> GetAllSchedulesAsync()
+        {
+            try
+            {
+                var scheduleRepository = _unitOfWork.GetRepository<DoctorSchedule>();
+                var schedules = await scheduleRepository.GetAllAsync("Doctor");
+                return _mapper.Map<IEnumerable<DoctorScheduleDTO>>(schedules);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting all schedules");
+                throw;
+            }
+        }
+    }
+}
---
+++ BusinessLogic/Services/Interfaces/IDoctorScheduleService.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLogic.DTOs.Doctor_Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.Interfaces
+{
+    public interface IDoctorScheduleService
+    {
+        Task<IEnumerable<DoctorScheduleDTO>> GetAllSchedulesAsync();
+       
+    }
+}

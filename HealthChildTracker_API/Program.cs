0e17610fb1be489ed8d6efabf171a0eaac0d4d19
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-02-27 11:01:56 +0700

    Configure doctorSchedule DI

--- HealthChildTracker_API/Program.cs
+++ HealthChildTracker_API/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IChildService, ChildService>();
 builder.Services.AddScoped<IGrowthRecordService, GrowthRecordService>();
 builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IDoctorScheduleService, DoctorScheduleService>();
 // Đăng ký automapper
 builder.Services.AddAutoMapper(typeof(MapperProfile));
 // cấu hình cors

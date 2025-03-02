9b8a7c4d8e2c1a6a496887fb71b6e3b7259551ea
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-03-03 02:45:40 +0700

    Add growth assessment logic: Cacul health child graphic

---
+++ BusinessLogic/DTOs/GrowthRecord/GrowthAssessmentDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTOs.GrowthRecord
+{
+    public class GrowthAssessmentDTO
+    {
+        public decimal ZScoreHeight { get; set; }
+        public decimal ZScoreWeight { get; set; }
+        public decimal ZScoreBMI { get; set; }
+        public decimal ZScoreHeadCircumference { get; set; }
+
+        public string HeightStatus { get; set; } // Bình thường/Thấp còi độ 1/Thấp còi độ 2...
+        public string WeightStatus { get; set; } // Bình thường/Suy dinh dưỡng/Thừa cân...
+        public string BMIStatus { get; set; } // Bình thường/Gầy/Béo phì độ 1/Béo phì độ 2...
+        public string HeadCircumferenceStatus { get; set; }
+
+        public string Recommendations { get; set; } // Lời khuyên tổng hợp
+    }
+}

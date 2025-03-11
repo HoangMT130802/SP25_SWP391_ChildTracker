e70395123d09714ed546cd1256f8a95545b637c3
trungntse173505 <trungntse173505@fpt.edu.vn>
2025-03-11 15:08:17 +0700

    add: CRUD Blog and Manager Blog for Admin

---
+++ BusinessLogic/Services/Implementations/BlogService.cs
@@ -0,0 +1,332 @@
+﻿using BusinessLogic.DTOs.Blog;
+using BusinessLogic.Services.Interfaces;
+using DataAccess.Entities;
+using DataAccess.Repositories;
+using DataAccess.UnitOfWork;
+using Microsoft.Extensions.Logging;
+
+namespace BusinessLogic.Services.Implementations
+{
+    public class BlogService : IblogService
+    {
+        private readonly IGenericRepository<Blog> _blogrepository;
+        private readonly ILogger<BlogService> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BlogService(IGenericRepository<Blog> blogrepository, ILogger<BlogService> logger, IUnitOfWork unitOfWork)
+        {
+            _blogrepository = blogrepository;
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        // Lấy allBlog chờ duyệt
+        public async Task<IEnumerable<BlogDTO>> GetAllBlogPendingAsync()
+        {
+            try
+            {
+                var blogs = await _blogrepository.FindAsync(b => b.Status == "Pending");
+
+                if (blogs == null || !blogs.Any())
+                {
+                    throw new Exception("No Blog in system.");
+                }
+                return blogs.Select(blog => new BlogDTO
+                {
+                    BlogId = blog.BlogId,
+                    AuthorId = blog.AuthorId,
+                    Title = blog.Title,
+                    Content = blog.Content,
+                    ImageUrl = blog.ImageUrl,
+                    Views = blog.Views,
+                    Likes = blog.Likes,
+                    CreatedAt = blog.CreatedAt
+                }).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to get list of blog", ex);
+            }
+        }
+
+
+        // Lấy allBlog đã dc duyệt
+        public async Task<IEnumerable<BlogDTO>> GetAllBlogApprovedAsync()
+        {
+            try
+            {
+                var blogs = await _blogrepository.FindAsync(b => b.Status == "Approved");
+
+                if (blogs == null || !blogs.Any())
+                {
+                    throw new Exception("No Blog in system.");
+                }
+                return blogs.Select(blog => new BlogDTO
+                {
+                    BlogId = blog.BlogId,
+                    AuthorId = blog.AuthorId,
+                    Title = blog.Title,
+                    Content = blog.Content,
+                    ImageUrl = blog.ImageUrl,
+                    Views = blog.Views,
+                    Likes = blog.Likes,
+                    CreatedAt = blog.CreatedAt
+                }).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to get list of blog", ex);
+            }
+        }
+        // lấy allblog có phân trang (đã dc duyệt)
+        public async Task<PaginatedList<BlogDTO>> GetAllBlogPaginatedAsync(int pageIndex, int pageSize)
+        {
+            try
+            {
+                var query = _blogrepository.GetAllQueryable().Where(b => b.Status == "Approved");
+
+                if (!query.Any())
+                {
+                    _logger.LogWarning("No Blog in system.");
+                    return new PaginatedList<BlogDTO>(new List<BlogDTO>(), 0, pageIndex, pageSize);
+                }
+                // Chuyển Blog -> BlogDTO trước khi phân trang
+                var blogDTOQuery = query.Select(blog => new BlogDTO
+                {
+                    BlogId = blog.BlogId,
+                    AuthorId = blog.AuthorId,
+                    Title = blog.Title,
+                    Content = blog.Content,
+                    ImageUrl = blog.ImageUrl,
+                    Views = blog.Views,
+                    Likes = blog.Likes,
+                    CreatedAt = blog.CreatedAt
+                });
+                // Phân trang 
+                var paginatedBlogs = await Task.Run(() => PaginatedList<BlogDTO>.Create(blogDTOQuery, pageIndex, pageSize));
+                return paginatedBlogs;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error when getting list of paginated blog.");
+                return new PaginatedList<BlogDTO>(new List<BlogDTO>(), 0, pageIndex, pageSize);
+            }
+        }
+
+        //  Blog theo ID
+        public async Task<BlogDTO> GetblogByIdAsync(int blogId)
+        {
+            try
+            {
+                var blog = await _blogrepository.GetAsync(b => b.BlogId == blogId && b.Status == "Approved");
+                if (blog == null)
+                {
+                    throw new Exception($"No Blog {blogId} in system.");
+                }
+                return new BlogDTO
+                {
+                    BlogId = blog.BlogId,
+                    AuthorId = blog.AuthorId,
+                    Title = blog.Title,
+                    Content = blog.Content,
+                    ImageUrl = blog.ImageUrl,
+                    Views = blog.Views,
+                    Likes = blog.Likes,
+                    CreatedAt = blog.CreatedAt
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while processing blog {blogId}");
+                throw;
+            }
+        }
+
+        // Blog theo từ khóa
+        public async Task<List<BlogDTO>> SearchBlogByKeyword(string keyword)
+        {
+            try
+            {
+                keyword = keyword.ToLower();
+                // Chỉ tìm blog có Status là "Approved" và chứa từ khóa trong Title hoặc Content
+                var blogkeyword = await _blogrepository.FindAsync(b =>
+                    b.Status == "Approved" &&
+                    (b.Title.ToLower().Contains(keyword) || b.Content.ToLower().Contains(keyword)));
+
+                if (!blogkeyword.Any())
+                {
+                    throw new Exception($"There are no posts containing the keyword '{keyword}'.");
+                }
+                return blogkeyword.Select(b => new BlogDTO
+                {
+                    BlogId = b.BlogId,
+                    AuthorId = b.AuthorId,
+                    Title = b.Title,
+                    Content = b.Content,
+                    ImageUrl = b.ImageUrl,
+                    Views = b.Views,
+                    Likes = b.Likes,
+                    CreatedAt = b.CreatedAt
+                }).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error during search: {ex.Message}");
+            }
+        }
+  
+
+        // Kiểm duyệt nội dung 
+        public async Task<bool> ApproveBlogAsync(int blogId)
+        {
+            try
+            {
+                var blog = await _blogrepository.GetByIdAsync(blogId);
+                if (blog.Status == "Pending")
+                {
+                    blog.Status = "Approved";
+                    _blogrepository.Update(blog);
+                    await _unitOfWork.SaveChangesAsync();
+                    return true;
+                }
+
+
+                throw new Exception($"Blog {blogId} previously approved.");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error while browsing article {blogId}: {ex.Message}");
+
+            }
+        }
+        public async Task<bool> RejectBlogAsync(int blogId)
+        {
+            try
+            {
+                var blog = await _blogrepository.GetByIdAsync(blogId);
+                if (blog.Status == "Pending")
+                {
+                    blog.Status = "Rejected";
+                    _blogrepository.Update(blog);
+                    await _unitOfWork.SaveChangesAsync();
+                    return true;
+                }
+                throw new Exception($"Blog {blogId} previously rejected.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error when rejecting article {blogId}: {ex.Message}");
+                return false;
+            }
+        }
+
+
+        // tạo blog
+        public async Task<BlogDTO> CreateBlogAsync(int userId, CreateBlogDTO createBlog)
+        {
+            try
+            {
+                var blog = new Blog
+                {
+                    AuthorId = userId,
+                    Title = createBlog.Title,
+                    Content = createBlog.Content,
+                    ImageUrl = createBlog.ImageUrl,
+                    Views = 0, // Mặc định ban đầu 
+                    Likes = 0, // Mặc định ban đầu
+                    Status = "Pending",
+                    CreatedAt = DateTime.UtcNow
+                };
+                await _blogrepository.AddAsync(blog);
+
+                await _unitOfWork.SaveChangesAsync();
+
+                return new BlogDTO
+                {
+                    BlogId = blog.BlogId,
+                    AuthorId = blog.AuthorId,
+                    Title = blog.Title,
+                    Content = blog.Content,
+                    ImageUrl = blog.ImageUrl,
+                    Views = blog.Views,
+                    Likes = blog.Likes,
+                    CreatedAt = blog.CreatedAt
+                };
+            }
+            catch(Exception ex)
+            {
+                
+                throw new Exception($"Error When Creating Blog: {ex.Message}", ex);
+            }
+        }
+
+        // cập nhật blog
+        public async Task<BlogDTO> UpdateBlogAsync(int blogId, UpdateBlogDTO updateBlog, int currentUserId, string currentUserRole)
+        {
+            try
+            {
+                var blog = await _blogrepository.GetByIdAsync(blogId);
+                if (blog == null)
+                {
+                    throw new Exception($"No blog found {blogId}");
+                }
+
+                // Kiểm tra quyền: Admin thì bỏ qua, còn Doctor thì chỉ được sửa bài của chính họ
+                if (currentUserRole == "Doctor" && blog.AuthorId != currentUserId)
+                {
+                    throw new UnauthorizedAccessException("Bạn chỉ có thể cập nhật bài viết của mình.");
+                }
+
+
+                // Cập nhật blog
+                blog.Title = updateBlog.Title;
+                blog.Content = updateBlog.Content;
+                blog.ImageUrl = updateBlog.ImageUrl;
+                blog.Status = "Pending"; // Chuyển trạng thái về Pending để chờ duyệt
+
+                _blogrepository.Update(blog);
+                await _unitOfWork.SaveChangesAsync();
+
+                return new BlogDTO
+                {
+                    BlogId = blog.BlogId,
+                    AuthorId = blog.AuthorId,
+                    Title = blog.Title,
+                    Content = blog.Content,
+                    ImageUrl = blog.ImageUrl,
+                    Views = blog.Views,
+                    Likes = blog.Likes,
+                    CreatedAt = blog.CreatedAt
+                };
+            }
+            catch
+            {
+                throw new Exception("Error when update");
+            }
+        }
+
+        // xóa blog 
+        public async Task<bool> DeleteBlogAsync(int blogId)
+        {
+            try
+            {
+                var Blog = await _blogrepository.GetAsync(b => b.BlogId == blogId);
+
+                if (Blog == null)
+                {
+                    throw new Exception($"No blog found {blogId}");
+                }
+
+                _blogrepository.Delete(Blog);
+                await _unitOfWork.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw new Exception($"Error detele blog {blogId}");
+            }
+
+        }
+    }
+}
---
+++ HealthChildTracker_API/Controllers/BlogController.cs
@@ -0,0 +1,192 @@
+﻿using System.Security.Claims;
+using BusinessLogic.DTOs.Blog;
+using BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers
+{
+    [Route("api/blog")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IblogService _blogService;
+
+        public BlogController(IblogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet("AllBlogPending")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> GetAllBlogPendingAsync()
+        {
+            try
+            {
+                var blogs = await _blogService.GetAllBlogPendingAsync();
+                return Ok(blogs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+
+
+        [HttpGet("AllBlogApproved")]
+        public async Task<ActionResult> GetAllBlogApprovedAsync()
+        {
+            try
+            {
+                var blogs = await _blogService.GetAllBlogApprovedAsync();
+                return Ok(blogs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+
+
+        [HttpGet("{blogId}")]
+        public async Task<ActionResult> GetBlogById(int blogId)
+        {
+            try
+            {
+                var blog = await _blogService.GetblogByIdAsync(blogId);
+                return Ok(blog);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+
+        [HttpGet("paged")]
+        public async Task<ActionResult> GetAllBlogsPaged([FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 3)
+        {
+            try
+            {
+                var paginatedBlogs = await _blogService.GetAllBlogPaginatedAsync(pageIndex, pageSize);
+                // Tạo object chứa kết quả phân trang
+                var result = new
+                {
+                    PageIndex = pageIndex,                     // Trang hiện tại
+                    PageSize = pageSize,                       // Số lượng trên mỗi trang
+                    Blogs = paginatedBlogs               // Danh sách các bài viết
+                };
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+
+
+        [HttpGet("searchblogkeyword")]
+        public async Task<IActionResult> SearchBlogskeyword([FromQuery] string keyword)
+        {
+            try
+            {
+                var result = await _blogService.SearchBlogByKeyword(keyword);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+
+        [HttpPut("approve/{blogId}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> ApproveBlog(int blogId)
+        {
+            var result = await _blogService.ApproveBlogAsync(blogId);
+            if (result) return Ok($"Bài viết {blogId} đã được duyệt.");
+            return BadRequest($"Lỗi duyệt bài viết {blogId}.");
+        }
+
+        [HttpPut("reject/{blogId}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RejectBlog(int blogId)
+        {
+            var result = await _blogService.RejectBlogAsync(blogId);
+            if (result) return Ok($"Bài viết {blogId} đã từ chối.");
+            return BadRequest($"Lỗi từ chối bài viết {blogId}.");
+        }
+
+
+        [HttpPost("CreateBlog")]
+        [Authorize(Roles = "Admin,Doctor")]
+        public async Task<ActionResult> CreateBlogAsync([FromBody] CreateBlogDTO createBlog)
+        {
+            if (createBlog == null)
+            {
+                return BadRequest(new { message = "Dữ liệu không hợp lệ" });
+            }
+
+            // Lấy userId từ token
+            var userIdClaim = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim == null)
+            {
+                return Unauthorized(new { message = "Không tìm thấy thông tin người dùng" });
+            }
+
+            int userId = int.Parse(userIdClaim.Value);
+            var createdBlog = await _blogService.CreateBlogAsync(userId, createBlog);
+
+            return Ok(createdBlog);
+        }
+
+
+        [HttpPut("UpdateBlog/{blogId}")]
+        [Authorize(Roles = "Admin,Doctor")]
+        public async Task<ActionResult> UpdateBlogAsync(int blogId, [FromBody] UpdateBlogDTO updateBlog)
+        {
+            if (updateBlog == null)
+            {
+                return BadRequest(new { message = "Dữ liệu không hợp lệ" });
+            }
+
+            // Lấy UserId và Role từ JWT Token
+            int currentUserId = int.Parse(User.FindFirst("UserId")?.Value ?? "0");
+            string currentUserRole = User.FindFirst("role")?.Value ?? "";
+
+            try
+            {
+                // Chuyển xuống Service xử lý
+                await _blogService.UpdateBlogAsync(blogId, updateBlog, currentUserId, currentUserRole);
+                return Ok(new { message = "Cập nhật thành công" });
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Forbid(); // HTTP 403: Không có quyền
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+
+        [HttpDelete("{blogId}")]
+        [Authorize(Roles = "Admin,Doctor")]
+        public async Task<ActionResult> DeleteDoctor(int blogId)
+        {
+            try
+            {
+                await _blogService.DeleteBlogAsync(blogId);
+                return Ok(new { message = "Delete successful" });
+            }
+            catch
+            {
+                return BadRequest(new { message = "Delete failed" });
+            }
+        }
+    }
+}

d15700dfba20331f65df6c1be36e1417af5c1ee8
hoangmt202 <97585818+hoangmt202@users.noreply.github.com>
2025-03-13 09:54:04 +0700

    Add required in creategrowthrecord and updategrowthrecord

--- BusinessLogic/DTOs/GrowthRecord/CreateGrowthRecordDTO.cs
+++ BusinessLogic/DTOs/GrowthRecord/CreateGrowthRecordDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,16 @@ namespace BusinessLogic.DTOs.GrowthRecord
     public class CreateGrowthRecordDTO
     {
         public int ChildId { get; set; }
+        [Required]
+        [Range(30, 200, ErrorMessage = "Chiều cao phải từ 30cm đến 200cm")]
         public decimal Height { get; set; }
+
+        [Required]
+        [Range(2, 100, ErrorMessage = "Cân nặng phải từ 2kg đến 100kg")]
         public decimal Weight { get; set; }
-        public decimal HeadCircumference { get; set; }
+
+        [Required]
+        [Range(30, 100, ErrorMessage = "Chu vi đầu phải từ 30cm đến 100cm")]
         public string Note { get; set; }
     }
 }
--- BusinessLogic/DTOs/GrowthRecord/UpdateGrowthRecordDTO.cs
+++ BusinessLogic/DTOs/GrowthRecord/UpdateGrowthRecordDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,18 @@ namespace BusinessLogic.DTOs.GrowthRecord
 {
     public class UpdateGrowthRecordDTO
     {
+       {
+        [Required]
+        [Range(30, 200, ErrorMessage = "Chiều cao phải từ 30cm đến 200cm")]
         public decimal Height { get; set; }
+
+        [Required]
+        [Range(2, 100, ErrorMessage = "Cân nặng phải từ 2kg đến 100kg")]
         public decimal Weight { get; set; }
+
+        [Required]
+        [Range(30, 100, ErrorMessage = "Chu vi đầu phải từ 30cm đến 100cm")]
         public decimal HeadCircumference { get; set; }
-        public string Note { get; set; }
+         public string Note { get; set; }
     }
 }

f561c02862dc70faa2699c61360048df0517e9fc
trungntse173505 <trungntse173505@fpt.edu.vn>
2025-03-18 02:07:13 +0700

    fix dto Doctor

--- BusinessLogic/DTOs/Doctor/CreateDoctorDTO.cs
+++ BusinessLogic/DTOs/Doctor/CreateDoctorDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,37 @@ namespace BusinessLogic.DTOs.Doctor
 {
     public class CreateDoctorDTO
     {
-        public int UserId { get; set; } 
+        [Required(ErrorMessage = "Vui lòng nhập username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập họ tên")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập chuyên khoa")]
         public string Specialization { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập bằng cấp/chứng chỉ")]
         public string Qualification { get; set; }
-        public int Experience { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số giấy phép hành nghề")]
         public string LicenseNumber { get; set; }
+
+
+        public int Experience { get; set; }
         public string Biography { get; set; }
     }
 }
--- BusinessLogic/DTOs/Doctor/DoctorDTO.cs
+++ BusinessLogic/DTOs/Doctor/DoctorDTO.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogic.DTOs.User;
 
 namespace BusinessLogic.DTOs.Doctor
 {
-    public class DoctorDTO
-    {
-        public string FullName { get; set; } 
-        public DoctorProfileDTO DoctorProfile { get; set; }
-    }
+        public class DoctorDTO : BaseUserDTO
+        {
+            //user
+            public int UserId { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string FullName { get; set; }
+            public string Phone { get; set; }
+            public string Address { get; set; }
+            public string Role { get; set; }
+            public bool Status { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
 
+            //DoctorProfile
+            public string Specialization { get; set; }
+            public string Qualification { get; set; }
+            public string LicenseNumber { get; set; }
+            public string Experience { get; set; }
+            public string Biography { get; set; }
+            public double Rating { get; set; }
+            public int ConsultationCount { get; set; }
+            public decimal AverageRating { get; set; }
+            public int TotalRatings { get; set; }
+            public bool IsVerified { get; set; }
+        }
 }

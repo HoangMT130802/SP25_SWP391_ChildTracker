ee68faa1007df582dc0e705864ecb390ecca5f16
trungntse173505 <trungntse173505@fpt.edu.vn>
2025-03-19 18:24:03 +0700

    add IBlogService

---
+++ BusinessLogic/Services/Interfaces/IBlogService.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogic.DTOs.Blog;
+
+namespace BusinessLogic.Services.Interfaces
+{
+    public interface IBlogService
+    {
+        Task<BlogDTO> CreateBlogAsync(int authorId, CreateBlogDTO blogDto);
+        Task<BlogDTO> UpdateBlogAsync(int blogId, int userId, string userRole, UpdateBlogDTO blogDto);
+        Task DeleteBlogAsync(int blogId, int userId, string userRole);
+        Task<BlogDTO> GetBlogByIdAsync(int blogId);
+        Task<IEnumerable<BlogDTO>> GetAllBlogsAsync();
+        Task<IEnumerable<BlogDTO>> GetBlogsByAuthorIdAsync(int authorId);
+    }
+}
